ae33a06b9a06df9e221510aeed8adad7346cef17
Dewald <dewald@perfectcircle.co.za>
2018-08-13 08:47:48 +0200

    image uploader

---
+++ core/lib/refinery/version.rb
@@ -0,0 +1,20 @@
+module Refinery
+  class Version
+    @major = 3
+    @minor = 0
+    @tiny  = 6
+    @build = nil
+
+    class << self
+      attr_reader :major, :minor, :tiny, :build
+
+      def to_s
+        [@major, @minor, @tiny, @build].compact.join('.')
+      end
+
+      def required_ruby_version
+        '>= 2.0.0'
+      end
+    end
+  end
+end
---
+++ testing/lib/refinery/testing/controller_macros/authentication.rb
@@ -0,0 +1,23 @@
+module Refinery
+  module Testing
+    module ControllerMacros
+      module Authentication
+
+        def refinery_login_with(*roles)
+          Refinery.deprecate('refinery_login_with', when: '3.1', replacement: 'refinery_login')
+          refinery_login
+        end
+
+        def refinery_login_with_factory(factory)
+          Refinery.deprecate('refinery_login_with_factory', when: '3.1', replacement: 'refinery_login')
+          refinery_login
+        end
+
+        def refinery_login
+          let(:logged_in_user) { Refinery::Core::NilUser.new }
+        end
+
+      end
+    end
+  end
+end
